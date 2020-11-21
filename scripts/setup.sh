0fc879a857d78e35b85c0e6f2697f1c7cff99c11
mzamoras <mzamoras@backlogics.com>
2020-11-21 06:12:16 -0600

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod -x install.sh
 sh install.sh -y
 wget https://raw.githubusercontent.com/mzamoras/do-setup/main/scripts/.zsh_utils -O ~/.zsh_utils
-echo "source ~/.zsh_utils" >> ~/.zshrc
+echo "source .zsh_utils" >> ~/.zshrc
