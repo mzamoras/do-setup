eb5a7d29e9c264cf6c43b6eb1fddcfda661c0933
Mike Zamora <mzamoras@backlogics.com>
2020-11-21 13:32:46 -0600

    option changed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update && apt-get -y install zsh
-$ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --keep-zshrc
+$ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"--keep-zshrc
 wget https://raw.githubusercontent.com/mzamoras/do-setup/main/scripts/.zsh_utils -O ~/.zsh_utils
 echo "source .zsh_utils" >> ~/.zshrc
 mkdir containers
