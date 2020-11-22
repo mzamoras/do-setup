4a4f69a3869504681257dd375658f7eab6664c6a
Mike Zamora <mzamoras@backlogics.com>
2020-11-22 04:16:28 -0600

    prompt for ohmyzsh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@ REPO="$( cd "$( dirname "$0" )" && pwd )"
 apt-get update && apt-get -y install zsh
 
 #GET OH-MY-ZSH
+if read -q '?Install Oh-my-zsh ?'; then
 wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ./installer.sh
 chmod -x installer.sh
+sh installer.sh
+else
+    echo "Skipping installing oh-my-zsh"
+fi
 
 #COPY THE ALIASES
-sh installer.sh
-
 if read -q '?Add variables to the shell (y/n) ?'; then
     echo "NGINX_DIR=$REPO/containers/nginx" >> ~/.zshrc
     echo "JENKINS_DIR=$REPO/containers/jenkins" >> ~/.zshrc
