be85a1b3de0132cfaef10ba8d8f3be3683d775cc
Mike Zamora <mzamoras@backlogics.com>
2020-11-22 03:10:04 -0600

    minor fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+REPO="$( cd "$( dirname "$0" )" && pwd )"
 #UPDATE
 apt-get update && apt-get -y install zsh
 
@@ -6,13 +7,15 @@ apt-get update && apt-get -y install zsh
 wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ./installer.sh
 chmod -x installer.sh
 
-#SETUP FOR DOCKER DAEMON
-sh ./daemon.sh
-
 #COPY THE ALIASES
 sh installer.sh
-REPO="$( cd "$( dirname "$0" )" && pwd )"
+read -p "Press enter to continue"
 echo "NGINX_DIR=$REPO/containers/nginx" >> ~/.zshrc
 echo "JENKINS_DIR=$REPO/containers/jenkins" >> ~/.zshrc
 echo "REPO=$REPO" >> ~/.zshrc
 echo "source $REPO/.zsh_utils" >> ~/.zshrc
+source $HOME/.zshrc
+
+#SETUP FOR DOCKER DAEMON
+read -p "Press enter to continue"
+sh ./daemon.sh
