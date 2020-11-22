34111328fc6a28de051db199645712a058885d83
Mike Zamora <mzamoras@backlogics.com>
2020-11-22 04:19:19 -0600

    wait for zsh availability

---
+++ scripts/installer.sh
@@ -0,0 +1,23 @@
+#GET OH-MY-ZSH
+if read -q '?Install Oh-my-zsh ?'; then
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ./installer.sh
+chmod -x installer.sh
+sh installer.sh
+else
+    echo "Skipping installing oh-my-zsh"
+fi
+
+#COPY THE ALIASES
+if read -q '?Add variables to the shell (y/n) ?'; then
+    echo "NGINX_DIR=$REPO/containers/nginx" >> ~/.zshrc
+    echo "JENKINS_DIR=$REPO/containers/jenkins" >> ~/.zshrc
+    echo "REPO=$REPO" >> ~/.zshrc
+    echo "source $REPO/scripts/.zsh_utils" >> ~/.zshrc
+else
+    echo "Skipping adding variables to oh-my-zsh"
+fi
+
+source $HOME/.zshrc
+
+#SETUP FOR DOCKER DAEMON
+source $REPO/scripts/daemon.sh
--- setup.sh
+++ setup.sh
@@ -3,26 +3,4 @@ REPO="$( cd "$( dirname "$0" )" && pwd )"
 #UPDATE
 apt-get update && apt-get -y install zsh
 
-#GET OH-MY-ZSH
-if read -q '?Install Oh-my-zsh ?'; then
-wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ./installer.sh
-chmod -x installer.sh
-sh installer.sh
-else
-    echo "Skipping installing oh-my-zsh"
-fi
-
-#COPY THE ALIASES
-if read -q '?Add variables to the shell (y/n) ?'; then
-    echo "NGINX_DIR=$REPO/containers/nginx" >> ~/.zshrc
-    echo "JENKINS_DIR=$REPO/containers/jenkins" >> ~/.zshrc
-    echo "REPO=$REPO" >> ~/.zshrc
-    echo "source $REPO/scripts/.zsh_utils" >> ~/.zshrc
-else
-    echo "Skipping adding variables to oh-my-zsh"
-fi
-
-source $HOME/.zshrc
-
-#SETUP FOR DOCKER DAEMON
-source $REPO/scripts/daemon.sh
+zsh ./scripts/installer.sh
